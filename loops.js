da8b21e6e35e1440b8bdefd4c25fbbf7d01f3faa
Alexander Keith Wilson <fonzmp@gmail.com>
2018-04-17 20:37:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ function forLoop(arr) {
     if (i === 1) {
       item.push("I am 1 strange loop.");
     } else {
-      console.log(`I am ${i} strange loops.`);
+      item.push(`I am ${i} strange loops.`);
     }
   }
   return item;
