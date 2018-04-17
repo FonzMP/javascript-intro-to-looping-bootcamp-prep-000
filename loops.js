e15d85706627754b417a5a84978452b0da45d328
Alexander Keith Wilson <fonzmp@gmail.com>
2018-04-17 20:58:01 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,8 +18,6 @@ function whileLoop(num){
   return 'done';
 }
 
-
-
 function doWhileLoop(array) {
   function maybeTrue() {
     return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
