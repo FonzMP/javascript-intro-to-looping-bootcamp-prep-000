ece9c6c3a031104ff6607e4cef1669c45a9ad3cb
Alexander Keith Wilson <fonzmp@gmail.com>
2018-04-17 20:35:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,15 +1,6 @@
 function forLoop(arr) {
   const item = new Array();
   for (let i = 0; i < 25; i++) {
-    if(i === 1) {
-      current = item.push('I am 1 strange loop.');
-      print(current);
-      return current;
-    } else {
-      current = item.push(`I am ${i} strange loops.`);
-      print(current);
-      return current;
-    }
+    console.log(`I am ${i} strange loops.`);
   }
-  return item;
 }
