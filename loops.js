35f741fb9f4a0c66e2ac118d017a2ea42179a66b
Alexander Keith Wilson <fonzmp@gmail.com>
2018-04-17 20:26:59 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,5 @@
 function forLoop(item) {
   for (var i = 0; i < 25; i++) {
-    if(i = 1){
-      console.log('1');
-      i++
-    } else {
-      console.log(i)
-    }
+    console.log(i);
   }
 }
